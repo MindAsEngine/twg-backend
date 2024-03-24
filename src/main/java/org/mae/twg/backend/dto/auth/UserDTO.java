4ebf27dd3ae965d4b578663caed0e61fcd1c1fee
SAPIPA <lbvf20035@gmail.com>
2024-03-24 21:19:09 +0300

    Delete unused method

--- src/main/java/org/mae/twg/backend/dto/auth/UserDTO.java
+++ src/main/java/org/mae/twg/backend/dto/auth/UserDTO.java
@@ -13,22 +13,15 @@ import org.mae.twg.backend.models.auth.User;
 @Schema(description = "Профиль пользователя")
 public class UserDTO {
     @Schema(description = "Логин пользователя", example = "Sapipa")
-    @Size(min = 5, max = 50, message = "Логин пользователя должно содержать от 5 до 50 символов")
-    @NotBlank(message = "Логин пользователя не может быть пустыми")
     private String username;
     @Schema(description = "Адрес электронной почты", example = "jondoe@gmail.com")
-    @Size(min = 5, max = 255, message = "Адрес электронной почты должен содержать от 5 до 255 символов")
-    @NotBlank(message = "Адрес электронной почты не может быть пустыми")
     @Email(message = "Email адрес должен быть в формате user@example.com")
     private String email;
     @Schema(description = "Номер телефона пользователя", example = "88005553535")
-    @NotBlank(message = "Номер телефона пользователя не может быть пустыми")
     private String phone;
     @Schema(description = "Имя пользователя", example = "Dima")
-    @NotBlank(message = "Имя пользователя не может быть пустыми")
     private String firstName;
     @Schema(description = "Фамилия пользователя", example = "Garaj")
-    @NotBlank(message = "Фамилия пользователя не может быть пустыми")
     private String lastName;
     @Schema(description = "Отчество пользователя", example = "Valerich")
     private String patronymic;
