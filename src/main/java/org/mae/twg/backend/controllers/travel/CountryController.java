c0a2889cc619ebee9cb3b5710f170b8c53aa1596
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2024-03-24 16:51:11 +0300

    update request DTOs and add geo data filling

--- src/main/java/org/mae/twg/backend/controllers/travel/CountryController.java
+++ src/main/java/org/mae/twg/backend/controllers/travel/CountryController.java
@@ -4,11 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import jakarta.validation.ValidationException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.mae.twg.backend.dto.travel.request.CountryRequestDTO;
+import org.mae.twg.backend.dto.travel.request.geo.CountryGeoDTO;
+import org.mae.twg.backend.dto.travel.request.locals.CountryLocalDTO;
 import org.mae.twg.backend.models.travel.enums.Localization;
 import org.mae.twg.backend.services.travel.CountryService;
 import org.springframework.http.ResponseEntity;
@@ -22,10 +21,22 @@ import java.util.List;
 @RequestMapping("/travel/{local}/countries")
 @Tag(name = "Страны")
 @Log4j2
-public class CountryController extends BaseTravelController<CountryService, CountryRequestDTO, CountryRequestDTO> {
+public class CountryController extends BaseTravelController<CountryService, CountryLocalDTO, CountryLocalDTO> {
     public CountryController(CountryService service) {
         super(service);
     }
+
+    @PostMapping("/{id}/geo/update")
+    @Operation(summary = "Установка новых геоднанных",
+            parameters = @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "JWT токен", required = true, example = "Bearer <token>")
+    )
+    public ResponseEntity<?> updateGeo(@PathVariable Localization local,
+                                       @PathVariable Long id,
+                                       @RequestBody CountryGeoDTO geoData) {
+        log.info("Set geo data for country with id = " + id);
+        return ResponseEntity.ok(getService().updateGeo(id, geoData, local));
+    }
+
     @PostMapping("/{id}/images/upload")
     @Operation(summary = "Добавить фотографии",
             parameters = @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "JWT токен", required = true, example = "Bearer <token>")
