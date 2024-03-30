e8b7ff03ced484a37540d8fee97cd52f436fcf6b
olshanskyvv <90987935+olshanskyvv@users.noreply.github.com>
2024-03-30 21:57:19 +0300

    add hospital DTOs

---
+++ src/main/java/org/mae/twg/backend/dto/travel/request/locals/HospitalLocalDTO.java
@@ -0,0 +1,22 @@
+package org.mae.twg.backend.dto.travel.request.locals;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.mae.twg.backend.dto.RequestDTO;
+
+@Data
+@AllArgsConstructor
+@Schema(description = "Запрос на локаль больницы")
+public class HospitalLocalDTO implements RequestDTO {
+    @Schema(description = "Название", example = "Какое-то название")
+    private String name;
+    @Schema(description = "Город", example = "Какой-то город")
+    private String city;
+    @Schema(description = "Введение", example = "Какое-то описание")
+    private String introduction;
+    @Schema(description = "Описание", example = "Какое-то описание")
+    private String description;
+    @Schema(description = "Адрес", example = "Какой-то адрес")
+    private String address;
+}
